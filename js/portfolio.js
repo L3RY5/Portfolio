2c9c063cdb79dae63a44cb2a65ecd23d35e9c3e0
L3RY5 <lerys91@hotmail.com>
2018-10-23 15:51:29 +0200

    popUp fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -47,6 +47,7 @@ $(document).ready(function () {
      span.onclick = function () {
         modal.style.display = "none";
     }
+    
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
