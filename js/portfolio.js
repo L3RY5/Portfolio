7fff80f01d53d29e8bc2485d43e5c730d0513145
L3RY5 <lerys91@hotmail.com>
2018-11-20 06:13:31 +0100

    validatie implementeren bij form

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,16 +1,37 @@
 $(document).ready(function () {
-$(".container2").on("click",function () {
-    /*
-    var id =$(this).attr("id");
-    console.log(id);*/
-  
- 
-})
+    var cssError={
+        "border": "red solid 2px",
+        "background-color":"gray",
+        "color":"white"
+    };
+    $("#submit").on("click", function () {
+
+        if ($.trim($('#first-name').val()) == '') {
+            /*$('#first-name').addClass("leeg");*/
+            alert("Voornaam is leeg");
+            
+           
+            
+        }else if ($.trim($('#last-name').val()) == '') {
+            alert("Achternaam is leeg");
+        }else if ($.trim($('#email').val()) == '') {
+            alert("Email is leeg");
+        }else if ($.trim($('#phone').val()) == '') {
+            alert("Telepoon nr is leeg");
+        }
+       
+        if ($('#first-name').val().match(/[^a-zA-Z]/g)) {
+            while ($('#first-name').val().match(/[^a-zA-Z]/g)) {
+                $("#first-name").css(cssError);
+                $("#first-name").attr("placeholder", "Deze veld kan alleen letters bevatten");
+            }
+        }
+
+
+
+    })
 
 
 
 
 });
-
-
-
